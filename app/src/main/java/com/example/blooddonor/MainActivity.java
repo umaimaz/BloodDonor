1b3d51da260b3722c28cae69dbde3b8ed2783772
UmaimaZulfiqar <bitf16m548@pucit.edu.pk>
2019-11-22 20:17:09 +0500

    3rd commit

--- app/src/main/java/com/example/blooddonor/MainActivity.java
+++ app/src/main/java/com/example/blooddonor/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.blooddonor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,8 +17,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavView = findViewById(R.id.bottom_navigation);
-
-        private BottomNavigationView.OnNavigationItemSelectedListener navListener = 
+        bottomNavView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,new FindDonor_Fragment()).commit();
     }
+        private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectFragment = null;
+
+                switch(menuItem.getItemId()){
+                    case R.id.find_donor:
+                        selectFragment = new FindDonor_Fragment();
+                        break;
+
+                    case R.id.request:
+                        selectFragment = new Request_Fragment();
+                        break;
+
+                    case R.id.request_for_blood:
+                        selectFragment = new RequestForBlood_Fragment();
+                        break;
+
+                    case R.id.history:
+                        selectFragment = new History_Fragment();
+                        break;
+
+                    case R.id.profile:
+                        selectFragment = new Profile_Fragment();
+                        break;
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,selectFragment).commit();
+                return true;
+            }
+        };
+
+
 
 }
