8a12affe172c35462c9d7d29e17c510cc0caa247
UmaimaZulfiqar <bitf16m548@pucit.edu.pk>
2019-11-22 19:30:14 +0500

    2nd commit

--- app/src/main/java/com/example/blooddonor/MainActivity.java
+++ app/src/main/java/com/example/blooddonor/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hi arslan
+
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_navigation);
+
+        private BottomNavigationView.OnNavigationItemSelectedListener navListener = 
     }
+
 }
